fdf082cae4cdaaf17a96ce08449bb3c8a9912c4f
unknown <sayidhosseini@hotmail.com>
2019-05-27 16:11:39 +0430

    fix issue with deleteUser regarding token

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
     dbURL: "mongodb://mongo:27017/auth",
     AuthenticationList: [{
+        method: "GET",
+        url: "/auth/v1/validate/token"
+    }, {
         method: "PUT",
         url: "/auth/v1/user/changePassword"
-    }, {
-        method: "DELETE",
-        url: "/auth/v1/user/logout"
     }, {
         method: "GET",
         url: "/auth/v1/user/list"
@@ -16,7 +16,10 @@ module.exports = {
         method: "PUT",
         url: "/auth/v1/user/role"
     }, {
-        method: "GET",
-        url: "/auth/v1/validate/token"
+        method: "DELETE",
+        url: "/auth/v1/user/logout"
+    }, {
+        method: "DELETE",
+        url: "/auth/v1/user/delete"
     }]
 };
